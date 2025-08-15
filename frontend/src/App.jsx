d1da7fa60dba906946a709c6a4e492c7d688a3db
raziquehasan <164611217+raziquehasan@users.noreply.github.com>
2025-08-15 10:36:55 +0530

    update login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { useAuth } from "./context/AuthProvider";
 
